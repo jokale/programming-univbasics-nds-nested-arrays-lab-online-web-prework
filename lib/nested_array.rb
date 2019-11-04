3a0a5bb064ec24fa287701baac0aabbed31d61fb
Joanna E K <kalemajoanna@gmail.com>
2019-11-04 17:29:25 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -33,7 +33,7 @@ end
 
 
 def matrix_update(matrix, row, column, new_value)
- ENNET_HOUSE [0][2] = "Tiny Ewell"
+ assembled_matrix [0][2] = "Tiny Ewell"
 end
   # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
